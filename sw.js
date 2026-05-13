a593f5d606713a9644efbeb163330c125761c46c
FuzeGSS <florent.duysens@gmail.com>
2026-05-13 10:08:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,37 @@
-const CACHE_NAME = "ffc-cache";
+const CACHE_NAME = "ffc-cache-v2";
 
 const FILES = [
   "./",
-  "./ffc_v4.html",
+  "./index.html",
   "./app.js",
   "./style.css"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
   );
 });
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
+``
